f4d5e959ec4b8e290953ea9d029c76e9fe09eb24
ForAeons <w.xy020203@gmail.com>
2023-07-04 21:49:12 +0800

    week 7 problems

--- Todos/Number of Islands.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-#  https://leetcode.com/problems/number-of-islands/
-
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
--- Todos/Product of Array Except Self.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-# https://leetcode.com/problems/product-of-array-except-self/
-
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
