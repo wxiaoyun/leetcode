827e7ffa21c571a08c75d4e0a9df9206fbf3e32b
ForAeons <w.xy020203@gmail.com>
2024-03-06 09:37:24 +0800

    Linked list cycle attempt 2

---
+++ finished/linked_list_cycle2.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/linked-list-cycle/description/
+# This is a reattempt of the problem.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head == None:
+            return False
+
+        fast = head
+        slow = head
+
+        while (slow.next != None and fast.next != None and fast.next.next != None):
+            slow = slow.next
+            fast = fast.next.next
+
+            if (slow == fast):
+                return True
+        
+        return False
