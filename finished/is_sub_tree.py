3468b4faa7a34c6e71ea667d4456a0bbe3080711
ForAeons <w.xy020203@gmail.com>
2024-05-11 15:12:52 +0800

    Is subtree

---
+++ finished/is_sub_tree.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/subtree-of-another-tree/
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == q:
+            return True
+        
+        if p == None or q == None:
+            return False
+        
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if subRoot == None:
+            return True
+        
+        if root == None:
+            return False
+        
+        return (
+            self.isSameTree(root, subRoot) or 
+            self.isSubtree(root.left, subRoot) or 
+            self.isSubtree(root.right, subRoot)
+        )
+        
