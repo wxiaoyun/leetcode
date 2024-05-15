86740bd077c8493cf7d1a003a4a9bb9e21c34773
ForAeons <w.xy020203@gmail.com>
2024-05-15 22:24:40 +0800

    Maximum happiness

---
+++ finished/maximum_happiness_sum.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximize-happiness-of-selected-children/
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(key=lambda h:-h)
+
+        i = 0
+        total_happiness = 0
+        for h in happiness:
+            total_happiness += max(0, h-i)
+            i+=1
+            if i == k:
+                break
+        return total_happiness    
