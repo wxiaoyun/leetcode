a0b9a053c322d73571abd9ccf0213c439c34ac35
ForAeons <w.xy020203@gmail.com>
2024-05-23 08:00:14 +0800

    Count good nodes in bin tree

---
+++ finished/count_good_nodes_in_binary_tree.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        # count = 1
+        # highest = {}
+        # highest[root] = root.val
+
+        # dq = deque()
+        # dq.append(root)
+
+        # while len(dq) > 0:
+        #     n = dq.popleft()
+
+        #     if n == None:
+        #         continue
+
+        #     for c in [n.left, n.right]:
+        #         if c == None:
+        #             continue
+        #         if c.val >= highest[n]:
+        #             count += 1
+        #         dq.append(c)
+        #         highest[c] = max(highest[n], c.val)
+        
+        # return count
+
+        def dfs(n: TreeNode, highest: int) -> int:
+            if n == None:
+                return 0
+            
+            g = 0
+
+            if n.val >= highest:
+                g+=1
+
+            highest_new=max(highest,n.val)            
+            return g+dfs(n.left,highest_new)+dfs(n.right,highest_new)
+
+        return dfs(root, root.val)
+        
