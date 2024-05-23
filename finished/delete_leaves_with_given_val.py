10373dd00e67e30f863e50298dd4468821b54035
ForAeons <w.xy020203@gmail.com>
2024-05-24 07:48:35 +0800

    Delete leaves given val

---
+++ finished/delete_leaves_with_given_val.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/delete-leaves-with-a-given-value/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if root == None:
+            return None
+
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if root.left == None and root.right == None and root.val == target:
+            return None
+        
+        return root
