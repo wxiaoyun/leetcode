ec0fff117ecc74e83b9bfb788d583ea29e8c883b
ForAeons <w.xy020203@gmail.com>
2024-05-31 08:06:16 +0800

    lc

---
+++ finished/count_triplet_xor_to_zero.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+
+class Solution:
+    def countTriplets(self, arr: List[int]) -> int:
+        prefix_xor = [0]
+
+        for n in arr:
+            prefix_xor.append(n^prefix_xor[-1])
+        
+        total = 0
+        
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                if not prefix_xor[j+1] ^ prefix_xor[i]:
+                    total += j-i
+        return total
+        
+        
