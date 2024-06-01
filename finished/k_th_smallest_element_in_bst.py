bf1eb1a30d4890c46c73c833151b36f429cbccb8
ForAeons <w.xy020203@gmail.com>
2024-06-02 07:52:11 +0800

    lc

---
+++ finished/k_th_smallest_element_in_bst.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.res = None
+        self.count = 0
+
+        def dfs_in_order(n: Optional[TreeNode], k: int):
+            if not n or self.res:
+                return
+            
+            dfs_in_order(n.left, k)
+            self.count += 1
+
+            if self.count == k:
+                self.res = n.val
+            
+            dfs_in_order(n.right, k)
+            
+        dfs_in_order(root, k)
+        
+        return self.res
