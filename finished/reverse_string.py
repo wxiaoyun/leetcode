bda0cb92ca80b113b8b271f91950757619094df5
ForAeons <w.xy020203@gmail.com>
2024-09-24 08:47:08 +0800

    lc

---
+++ finished/reverse_string.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/reverse-string/submissions/1274678451
+# This is actually an april fools question on leetcode
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
