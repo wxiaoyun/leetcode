fbc86a8b5fd5e5e32adda6ad8d2997dcca37750b
ForAeons <w.xy020203@gmail.com>
2024-11-19 09:40:26 +0800

    revisit

--- finished/minimum_total_distance_traveled.py
+++ finished/minimum_total_distance_traveled.py
@@ -2,8 +2,42 @@ from typing import Dict, List, Tuple
 
 # https://leetcode.com/problems/minimum-total-distance-traveled/
 
-
 class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+      robot.sort()
+      factory.sort()
+      R = len(robot)
+      F = len(factory)
+
+      dp = {}
+      def helper(r: int, f: int, fcnt: int) -> int:
+        if r >= R:
+          return 0
+        
+        if f >= F:
+          return float('inf')
+        
+        if fcnt >= factory[f][1]:
+          return helper(r, f+1, 0)
+        
+        key = (r, f, fcnt)
+        if key in dp:
+          return dp[key]
+        
+        rpos = robot[r]
+        fpos = factory[f][0]
+
+        # Fix at current factory
+        best = abs(rpos-fpos) + helper(r+1, f, fcnt+1)
+
+        # Skip current factory
+        best = min(best, helper(r, f, fcnt+1))
+
+        dp[key] = best
+        return best
+      
+      return helper(0, 0, 0)
+    
     def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
         robot.sort()
         factory.sort()
