8e3b557491ffaf72a267ef3f23b10727e1d730bb
ForAeons <w.xy020203@gmail.com>
2024-11-30 17:47:45 +0800

    lc

---
+++ finished/boats_to_save_people.py
@@ -0,0 +1,18 @@
+from typing import List
+
+# https://leetcode.com/problems/boats-to-save-people/description
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        
+        left = 0
+        right = len(people) - 1
+        boats = 0
+
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            boats += 1
+        return boats
