ab65f237ce2e25f848059b3499b9bfdc7d2e925d
ForAeons <w.xy020203@gmail.com>
2024-12-25 09:49:54 +0800

    revisit

--- finished/find_minimum_diameter_after_merging_two_trees.py
+++ finished/find_minimum_diameter_after_merging_two_trees.py
@@ -4,7 +4,42 @@ from typing import List
 
 # https://leetcode.com/problems/find-minimum-diameter-after-merging-two-trees/
 
+
 class Solution:
+    def minimumDiameterAfterMerge(
+        self, edges1: List[List[int]], edges2: List[List[int]]
+    ) -> int:
+        adj1 = defaultdict(list)
+        for a, b in edges1:
+            adj1[a].append(b)
+            adj1[b].append(a)
+
+        adj2 = defaultdict(list)
+        for u, v in edges2:
+            adj2[u].append(v)
+            adj2[v].append(u)
+
+        def get_diameter(adj, cur, par):
+            diam = -1
+            child_depths = [0, 0]
+
+            for nb in adj[cur]:
+                if nb == par:
+                    continue
+
+                diam_est, depth = get_diameter(adj, nb, cur)
+                diam = max(diam, diam_est)
+                heapq.heappush(child_depths, depth)
+                heapq.heappop(child_depths)
+
+            diam = max(diam, sum(child_depths))
+            return diam, 1 + max(child_depths)
+
+        diam_a, _ = get_diameter(adj1, 0, -1)
+        diam_b, _ = get_diameter(adj2, 0, -1)
+        diam_new = (diam_a + 1) // 2 + (diam_b + 1) // 2 + 1
+        return max(diam_a, diam_b, diam_new)
+
     def minimumDiameterAfterMerge(
         self, edges1: List[List[int]], edges2: List[List[int]]
     ) -> int:
