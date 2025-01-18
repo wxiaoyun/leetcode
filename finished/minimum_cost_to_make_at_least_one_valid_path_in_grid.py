49652d9768bee2066598a77aa74678756585e49d
ForAeons <w.xy020203@gmail.com>
2025-01-18 10:09:35 +0800

    lc

---
+++ finished/minimum_cost_to_make_at_least_one_valid_path_in_grid.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+import heapq
+from typing import List, Tuple
+
+
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        def nxt(i: int, j: int, d: int) -> Tuple[int, int]:
+            mp = {
+                1: (0, 1),
+                2: (0, -1),
+                3: (1, 0),
+                4: (-1, 0)
+            }
+            di, dj = mp[d]
+            return i + di, j + dj
+
+        R = len(grid)
+        C = len(grid[0])
+        dest = (R - 1, C - 1)
+
+        adj_list = defaultdict(list)
+        for r in range(R):
+            for c in range(C):
+                cur_d = grid[r][c]
+                for d in range(1, 5):
+                    rr, cc = nxt(r, c, d)
+                    
+                    if min(rr, cc) < 0 or rr >= R or cc >= C:
+                        continue
+
+                    cost = 1 if d != cur_d else 0
+                    adj_list[(r, c)].append((rr, cc, cost))
+        
+        # print(adj_list) 
+
+        visited = set()
+        pq = [(0, 0, 0)]
+
+        while pq:
+            cost, r, c = heapq.heappop(pq)
+            cost = cost
+            node = (r, c)
+
+            if node in visited:
+                continue
+            visited.add(node)
+
+            if node == dest:
+                return cost
+            
+            for rr, cc, edge_cost in adj_list[node]:
+                heapq.heappush(pq, (cost + edge_cost, rr, cc))
+        
+        return -1
