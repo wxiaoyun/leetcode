d29dbf614eeec3fe13e4696bfac88c21300c497f
xiaoyunwu.w <xiaoyunwu.w@bytedance.com>
2025-01-19 10:50:53 +0800

    revisit

--- finished/check_if_a_parentheses_string_can_be_valid.py
+++ finished/check_if_a_parentheses_string_can_be_valid.py
@@ -6,6 +6,43 @@ class Solution:
         if len(s) % 2 != 0:
             return False
 
+        stack = []
+        free = []
+
+        for i in range(len(s)):
+            ch = s[i]
+            is_locked = locked[i] == "1"
+
+            if not is_locked:
+                free.append(i)
+                continue
+
+            if ch == "(":
+                stack.append(i)
+                continue
+
+            # ch == ")"
+            if stack:
+                stack.pop()
+                continue
+
+            # stack empty
+            if free:
+                free.pop()  # consume a free bracket for "("
+                continue
+
+            return False
+
+        while stack and free and stack[-1] < free[-1]:
+            stack.pop()
+            free.pop()  # use the free backet for ")"
+
+        return len(stack) == 0
+
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
         free = []
         brackets = []
 
---
+++ finished/construct_k_palindrome_strings.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+# https://leetcode.com/problems/construct-k-palindrome-strings/
+
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+
+        cnt = Counter(s)
+
+        odds = 0
+        for _, fq in cnt.items():
+            if fq % 2 == 1:
+                odds += 1
+
+        if odds > k:
+            return False
+        return True
+
+    def canConstruct(self, s: str, k: int) -> bool:
+        N = len(s)
+
+        if k > N:
+            return False
+
+        if k == N:
+            return True
+
+        cnt = Counter(s)
+        odds = 0
+        for _, fq in cnt.items():
+            if fq % 2 == 1:
+                odds += 1
+
+        if k < odds:
+            return False
+
+        return True
