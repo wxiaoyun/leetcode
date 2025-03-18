42b940aec1b238133ae63d0a3034af7244851d52
wxiaoyun <w.xy020203@gmail.com>
2025-03-18 08:59:48 +0800

    lc

--- src/python/finished/longest_nice_subarray.py
+++ src/python/finished/longest_nice_subarray.py
@@ -20,3 +20,21 @@ class Solution:
             best = max(best, len(q))
 
         return best
+
+# Space optimised
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        best = 1
+        cur = 0
+        l = 0
+        for r, n in enumerate(nums):
+            # cur & test should not overlap: == 0
+            while (cur & n) != 0:
+                left = nums[l]
+                cur = cur ^ left
+                l += 1
+            
+            cur = cur ^ n
+            best = max(best, r - l + 1)
+        
+        return best
