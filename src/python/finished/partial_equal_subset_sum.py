6ce90421e41e6d71ff401364c1191b5e4dee8cb8
wxiaoyun <w.xy020203@gmail.com>
2025-04-07 08:29:13 +0800

    lc

--- src/python/finished/partial_equal_subset_sum.py
+++ src/python/finished/partial_equal_subset_sum.py
@@ -2,6 +2,23 @@ from typing import List
 
 # https://leetcode.com/problems/partition-equal-subset-sum/
 
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        
+        diffs = set([0]) 
+        for n in nums:
+            new_diffs = set()
+            for d in diffs:
+                new_diffs.add(abs(n - d)) 
+                new_diffs.add(n + d) 
+            diffs = new_diffs
+            
+        return 0 in diffs
+
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
