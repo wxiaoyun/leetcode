a9e30dfd2f9ed184c3fb1a74f3b99d5584a0aad3
wxiaoyun <w.xy020203@gmail.com>
2025-05-03 09:10:17 +0800

    lc

---
+++ src/python/finished/minimum_domino_rotations_for_equal_row.py
@@ -0,0 +1,35 @@
+from typing import List
+
+# https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+
+
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        N = len(tops)
+        cur = (1 << 8) - 1
+        for i in range(N):
+            local = (1 << tops[i]) | (1 << bottoms[i])
+            cur = cur & local
+
+            if cur == 0:
+                return -1
+
+        bcur = list(bin(cur)[2:])
+        bcur.reverse()
+
+        best = float("inf")
+        for digit, bit in enumerate(bcur):
+            if bit != "1":
+                continue
+
+            target = int(bit) << digit
+            count_top, count_bottom = 0, 0
+            for i in range(N):
+                if (1 << tops[i]) != target:
+                    count_top += 1
+                if (1 << bottoms[i]) != target:
+                    count_bottom += 1
+
+            best = min(best, count_top, count_bottom)
+
+        return best
