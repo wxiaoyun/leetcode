e1673fbc885b6f7dee686cac492604533d7177b3
wxiaoyun <w.xy020203@gmail.com>
2025-05-11 07:37:31 +0900

    lc

---
+++ src/python/finished/minimum_equal_sum_of_two_rrays_after_replacing_zeros.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  long long minSum(vector<int>& nums1, vector<int>& nums2) {
+    long long ltotal = 0;
+    long long rtotal = 0;
+    long long lzero = 0;
+    long long rzero = 0;
+
+    for (const auto n : nums1) {
+      ltotal += n;
+      if (n == 0) {
+        lzero++;
+      }
+    }
+
+    for (const auto n : nums2) {
+      rtotal += n;
+      if (n == 0) {
+        rzero++;
+      }
+    }
+
+    auto lmin = ltotal + lzero;
+    auto rmin = rtotal + rzero;
+
+    if (lmin < rmin && lzero == 0) {
+      return -1;
+    }
+
+    if (rmin < lmin && rzero == 0) {
+      return -1;
+    }
+
+    return lmin > rmin ? lmin : rmin;
+  }
+};
