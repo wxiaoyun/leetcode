5ac9982dc93ae1e0a8216eae4eee882d4c580095
wxiaoyun <w.xy020203@gmail.com>
2025-06-06 09:02:51 +0800

    lc

---
+++ src/python/finished/using_a_robot_to_print_the_lexicographically_smallest_string.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string
+
+
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        # for every character in s:
+        # append to t:
+        # - Choice 1: immediately add to p:
+        # - Choice 2: wait and see if theres smaller character
+
+        N = len(s)
+        pos_min = [N - 1] * N
+        for i in reversed(range(N - 1)):
+            if s[pos_min[i + 1]] < s[i]:
+                pos_min[i] = pos_min[i + 1]
+            else:
+                pos_min[i] = i
+
+        t = []
+        p = []
+        for i, ch in enumerate(s):
+            t.append(ch)
+
+            while t and (i == N - 1 or (t[-1] <= s[pos_min[i + 1]])):
+                p.append(t.pop())
+
+        while t:
+            p.append(t.pop())
+
+        return "".join(p)
