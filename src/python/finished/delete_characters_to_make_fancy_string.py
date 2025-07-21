b9a5899f629bd4023f51afd7d0091ded33776aad
wxiaoyun <w.xy020203@gmail.com>
2025-07-21 08:07:46 +0800

    lc

--- src/python/finished/delete_characters_to_make_fancy_string.py
+++ src/python/finished/delete_characters_to_make_fancy_string.py
@@ -1,18 +1,29 @@
 # https://leetcode.com/problems/delete-characters-to-make-fancy-string
 
+
 class Solution:
     def makeFancyString(self, s: str) -> str:
         prev, pcnt = "", 0
         res = []
         for c in s:
-          if c == prev:
-            if pcnt < 2:
-              pcnt += 1
+            if c == prev:
+                if pcnt < 2:
+                    pcnt += 1
+                else:
+                    continue
             else:
-              continue
-          else:
-            prev = c
-            pcnt = 1
+                prev = c
+                pcnt = 1
 
-          res.append(c)
+            res.append(c)
         return "".join(res)
+
+
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        builder = [s[i] for i in range(min(2, len(s)))]
+        for i in range(2, len(s)):
+            if (s[i]) * 3 == s[i - 2 : i + 1]:
+                continue
+            builder.append(s[i])
+        return "".join(builder)
