67276ab2156d73686fba98d029dc1f5f2be368a7
wxiaoyun <w.xy020203@gmail.com>
2025-09-01 08:34:26 +0800

    revisit

--- src/python/finished/maximum_average_pass_ratio.py
+++ src/python/finished/maximum_average_pass_ratio.py
@@ -2,6 +2,38 @@ import heapq
 from typing import List
 
 # https://leetcode.com/problems/maximum-average-pass-ratio/
+import heapq
+
+
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        # (a + 1) / (a + b + 1)
+
+        # 1/2 = 0.5 | 2/3 = 0.66 | inc = 0.16
+        # 2/3 = 0.66 | 3/4 = 0.75 | inc = 0.0833333333
+        # the larger the base, the smaller the inc
+
+        # Increase is given by
+        # (a + 1) / (a + b + 1) - a/(a+b)
+        # = ((a+1)(a+b) - a(a+b+1)) / ((a+b)*(a+b+1))
+
+        def compute_inc(pas: int, total: int, inc: int = 1) -> float:
+            return ((pas + inc) * (total) - pas * (total + inc)) / (
+                (total) * (total + inc)
+            )
+
+        pq = [(-compute_inc(pas, total), pas, total) for pas, total in classes]
+        heapq.heapify(pq)
+
+        for _ in range(extraStudents):
+            _, pas, total = heapq.heappop(pq)
+            new_pas = pas + 1
+            new_total = total + 1
+            heapq.heappush(pq, (-compute_inc(new_pas, new_total), new_pas, new_total))
+
+        ratio_sum = sum(pas / total for _, pas, total in pq)
+        ratio_avg = ratio_sum / len(pq)
+        return ratio_avg
 
 
 class Solution:
