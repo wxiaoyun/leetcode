50a6d96785fd5a8d70cfd8268a35635e3ef35908
wxiaoyun <w.xy020203@gmail.com>
2025-09-15 09:03:28 +0800

    chore: fmt code

--- src/rust/finished/maximum_number_of_words_you_can_type.rs
+++ src/rust/finished/maximum_number_of_words_you_can_type.rs
@@ -1,20 +1,18 @@
 impl Solution {
-  pub fn can_be_typed_words(text: String, broken_letters: String) -> i32 {
-      let bad_mask = broken_letters.chars().fold(0_u32, |prev, cur| {
-          prev | 1 << (cur as u8) as u32
-      });
+    pub fn can_be_typed_words(text: String, broken_letters: String) -> i32 {
+        let bad_mask = broken_letters
+            .chars()
+            .fold(0_u32, |prev, cur| prev | 1 << (cur as u8) as u32);
 
-      text
-          .split_ascii_whitespace()
-          .map(|w| w.chars().fold(0_u32, |prev, cur| {
-              prev | 1 << (cur as u8) as u32
-          }))
-          .map(|mask| {
-              match mask & bad_mask {
-                  0 => 1,
-                  _ => 0,
-              }
-          })
-          .sum()
-  }
+        text.split_ascii_whitespace()
+            .map(|w| {
+                w.chars()
+                    .fold(0_u32, |prev, cur| prev | 1 << (cur as u8) as u32)
+            })
+            .map(|mask| match mask & bad_mask {
+                0 => 1,
+                _ => 0,
+            })
+            .sum()
+    }
 }
