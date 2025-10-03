800c210039ef408ee3294c404467fe3ac4678ad3
wxiaoyun <w.xy020203@gmail.com>
2025-10-03 09:25:26 +0800

    hard

--- src/python/revisit/trapping_rain_water_ii.py
+++ src/python/revisit/trapping_rain_water_ii.py
@@ -3,6 +3,48 @@ from typing import List
 
 # https://leetcode.com/problems/trapping-rain-water-ii/
 
+
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        R = len(heightMap)
+        C = len(heightMap[0])
+
+        boundary = set()
+        for r in range(R):
+            boundary.add((r, 0))
+            boundary.add((r, C - 1))
+        for c in range(C):
+            boundary.add((0, c))
+            boundary.add((R - 1, c))
+
+        boundary_pq = [(heightMap[r][c], r, c) for r, c in boundary]
+        heapq.heapify(boundary_pq)
+
+        volume = 0
+        while boundary_pq:
+            h, r, c = heapq.heappop(boundary_pq)
+
+            for nbr in [
+                (r - 1, c),
+                (r + 1, c),
+                (r, c - 1),
+                (r, c + 1),
+            ]:
+                rr, cc = nbr
+                if min(rr, cc) < 0 or rr >= R or cc >= C:
+                    continue
+                if nbr in boundary:
+                    continue
+
+                hh = heightMap[rr][cc]
+                volume += max(0, h - hh)
+
+                heapq.heappush(boundary_pq, (max(h, hh), rr, cc))
+                boundary.add((rr, cc))
+
+        return volume
+
+
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
         R = len(heightMap)
