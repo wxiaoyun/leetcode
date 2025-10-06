95d52d16183e2bab4cf28776365d29a7ad43229f
wxiaoyun <w.xy020203@gmail.com>
2025-10-06 14:25:54 +0800

    revisit

--- src/python/revisit/ways_to_express_an_integer_as_sum_of_powers.py
+++ src/python/revisit/ways_to_express_an_integer_as_sum_of_powers.py
@@ -17,3 +17,36 @@ class Solution:
                 dp[num] = (dp[num] + dp[num - p]) % MOD
 
         return dp[n]
+
+
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        MOD = int(1e9 + 7)
+
+        # key = <n, next_base>
+        def compute(dp: dict, n: int, next_base: int, x: int) -> int:
+            key = (n, next_base)
+            if key in dp:
+                return dp[key]
+
+            if n == 0:
+                return 1
+
+            next_pow = next_base**x
+            if next_pow > n:
+                return 0
+
+            ways = 0
+
+            # use next_base
+            ways += compute(dp, n - next_pow, next_base + 1, x)
+            ways %= MOD
+
+            # skip next_base
+            ways += compute(dp, n, next_base + 1, x)
+            ways %= MOD
+
+            dp[key] = ways
+            return ways
+
+        return compute({}, n, 1, x)
