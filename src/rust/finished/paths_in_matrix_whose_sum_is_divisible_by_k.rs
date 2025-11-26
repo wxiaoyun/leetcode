933bfc228e639dcc6a891c1886922de37fe6b0da
wxiaoyun <w.xy020203@gmail.com>
2025-11-26 09:23:54 +0800

    perf

--- src/rust/finished/paths_in_matrix_whose_sum_is_divisible_by_k.rs
+++ src/rust/finished/paths_in_matrix_whose_sum_is_divisible_by_k.rs
@@ -3,9 +3,9 @@ impl Solution {
         let k = k as usize;
         let m = grid.len() as usize;
         let n = grid[0].len() as usize;
-        let mut dp = vec![vec![0i32; k]; n];
-        let mut tmp = vec![vec![0i32; k]; n];
-        dp[n - 1][0] = 1;
+        let mut dp = vec![0i32; k * n];
+        let mut tmp = vec![0i32; k * n];
+        dp[(n - 1) * k] = 1;
         const modd: i32 = (1e9 as i32) + 7;
 
         for r in (0..m).rev() {
@@ -15,9 +15,9 @@ impl Solution {
                 let cell_val = grid[r][c] as usize;
                 for l in 0..k {
                     let m = (cell_val + l).rem_euclid(k);
-                    tmp[c][m] = {
-                        let prev_row_cnt = dp[c][l];
-                        let prev_col_cnt = if c + 1 < n { tmp[c + 1][l] } else { 0 };
+                    tmp[c * k + m] = {
+                        let prev_row_cnt = dp[c * k + l];
+                        let prev_col_cnt = if c + 1 < n { tmp[(c + 1) * k + l] } else { 0 };
                         (prev_row_cnt + prev_col_cnt).rem_euclid(modd)
                     }
                 }
@@ -25,6 +25,6 @@ impl Solution {
             std::mem::swap(&mut dp, &mut tmp);
         }
 
-        dp[0][0]
+        dp[0]
     }
 }
