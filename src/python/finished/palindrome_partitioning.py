2059bb8a778810852a158313a9f0706b71bfd952
wxiaoyun <w.xy020203@gmail.com>
2025-12-13 19:05:24 +0800

    revisit

--- src/python/finished/palindrome_partitioning.py
+++ src/python/finished/palindrome_partitioning.py
@@ -6,14 +6,43 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        def is_pal(s: str, l: int, r: int) -> bool:
+            while l < r:
+                if not s[l] == s[r]:
+                    return False
+
+                l += 1
+                r -= 1
+            return True
+
+        def gen(s: str, i: int, builder: List[str], output: List[List[str]]):
+            if i >= len(s):
+                output.append(builder[:])
+                return
+
+            for j in range(i, len(s)):
+                if not is_pal(s, i, j):
+                    continue
+                sub_str = s[i : j + 1]
+                builder.append(sub_str)
+                gen(s, j + 1, builder, output)
+                builder.pop()
+
+        partitions = []
+        gen(s, 0, [], partitions)
+        return partitions
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
         def is_pldm(s: str) -> bool:
             mid = len(s) // 2
-            for i in range(mid+1):
-                j = len(s)-1-i
+            for i in range(mid + 1):
+                j = len(s) - 1 - i
                 if not s[i] == s[j]:
                     return False
             return True
-        
+
         out = []
         unique = set()
         queue = deque()
@@ -28,24 +57,24 @@ class Solution:
             out.append(p)
 
             for i in range(len(p)):
-                if i+1 < len(p):
-                    l, r = p[i], p[i+1]
-                    merged = l+r
+                if i + 1 < len(p):
+                    l, r = p[i], p[i + 1]
+                    merged = l + r
                     if is_pldm(merged):
                         new_p = []
                         new_p.extend(p[:i])
                         new_p.append(merged)
-                        new_p.extend(p[i+2:])
+                        new_p.extend(p[i + 2 :])
                         queue.append(new_p)
-                    
-                if i+2 < len(p):
-                    l, m, r = p[i], p[i+1], p[i+2]
-                    merged = l+m+r
+
+                if i + 2 < len(p):
+                    l, m, r = p[i], p[i + 1], p[i + 2]
+                    merged = l + m + r
                     if is_pldm(merged):
                         new_p = []
                         new_p.extend(p[:i])
                         new_p.append(merged)
-                        new_p.extend(p[i+3:])
+                        new_p.extend(p[i + 3 :])
                         queue.append(new_p)
-        
+
         return [item for item in out]
