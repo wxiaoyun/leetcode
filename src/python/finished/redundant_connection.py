c1388a2e7e63b5263e1bdfbcb47d6c65af0166a6
wxiaoyun <w.xy020203@gmail.com>
2025-12-14 16:39:41 +0800

    revisit

--- src/python/finished/redundant_connection.py
+++ src/python/finished/redundant_connection.py
@@ -2,17 +2,65 @@ from typing import List
 
 # https://leetcode.com/problems/redundant-connection/
 
+
+class UF:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+        self.component = n
+
+    def find(self, a: int) -> int:
+        if a == self.parent[a]:
+            return a
+
+        self.parent[a] = self.find(self.parent[a])
+        return self.parent[a]
+
+    def union(self, a: int, b: int) -> None:
+        ap, bp = self.find(a), self.find(b)
+        if ap == bp:
+            return None
+
+        apr, bpr = self.rank[ap], self.rank[bp]
+        if apr > bpr:
+            self.parent[bp] = ap
+            self.rank[apr] += bpr
+        else:
+            self.parent[ap] = bp
+            self.rank[bpr] += apr
+
+        self.component -= 1
+        return None
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = 0
+        for a, b in edges:
+            n = max(n, a, b)
+
+        redundant = []
+        uf = UF(n + 1)
+        for a, b in edges:
+            if uf.find(a) == uf.find(b):
+                redundant = [a, b]
+                continue
+
+            uf.union(a, b)
+        return redundant
+
+
 class UF:
     def __init__(self):
         self.parent = {}
         self.rank = {}
-    
+
     def union(self, a: int, b: int) -> None:
         ap = self.find(a)
         bp = self.find(b)
         if ap == bp:
-          return None
-        
+            return None
+
         ar = self.rank[ap]
         br = self.rank[bp]
 
@@ -25,7 +73,7 @@ class UF:
             self.rank[ap] += 1
 
         return None
-    
+
     def find(self, a: int) -> int:
         if a not in self.parent:
             self.parent[a] = a
@@ -37,7 +85,7 @@ class UF:
 
         self.parent[a] = self.find(p)
         return self.parent[a]
-    
+
     def same_set(self, a: int, b: int) -> bool:
         return self.find(a) == self.find(b)
 
