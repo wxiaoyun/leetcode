2bf1876a940470546a72ac6e1fc0dceb54464943
wxiaoyun <w.xy020203@gmail.com>
2025-12-16 10:58:59 +0800

    revisit

--- src/python/finished/swim_in_rising_water.py
+++ src/python/finished/swim_in_rising_water.py
@@ -6,6 +6,39 @@ from typing import List
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        t = 0
+        pq = [(grid[0][0], 0, 0)]
+        visited = set()
+        while pq:
+            time, r, c = heapq.heappop(pq)
+            visited.add((r, c))
+            t = max(t, time)
+
+            if r == m - 1 and c == n - 1:
+                return t
+
+            for rr, cc in [
+                (r + 1, c),
+                (r - 1, c),
+                (r, c + 1),
+                (r, c - 1),
+            ]:
+                if min(rr, cc) < 0 or rr >= m or cc >= n:
+                    continue
+
+                if (rr, cc) in visited:
+                    continue
+
+                heapq.heappush(pq, (grid[rr][cc], rr, cc))
+
+        return -1
+
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
         R, C = len(grid), len(grid[0])
         dest = (R - 1, C - 1)
         visited = set()
