4a4d4eaded97a003dbb16cee8c63acdbb6c75437
wxiaoyun <w.xy020203@gmail.com>
2025-12-19 21:13:17 +0800

    revisit

--- src/python/finished/hand_of_straights.py
+++ src/python/finished/hand_of_straights.py
@@ -1,13 +1,34 @@
-# https://leetcode.com/problems/hand-of-straights
-
+from collections import Counter
 from typing import List
+
+# https://leetcode.com/problems/hand-of-straights
 
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if (len(hand)%groupSize) != 0:
+        hand = sorted(hand)
+        cnt = Counter(hand)
+
+        for n in hand:
+            if n not in cnt:
+                continue
+
+            for nn in range(n, n + groupSize):
+                if nn not in cnt:
+                    return False
+
+                cnt[nn] -= 1
+                if cnt[nn] == 0:
+                    del cnt[nn]
+
+        return True
+
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if (len(hand) % groupSize) != 0:
             return False
-        
+
         hand.sort()
         used = -1
 
@@ -22,14 +43,14 @@ class Solution:
         for i in range(len(hand)):
             if hand[i] == used:
                 continue
-            
+
             for j in range(1, groupSize):
-                found = find_and_consume(i+j, hand[i]+j)
+                found = find_and_consume(i + j, hand[i] + j)
                 if not found:
                     return False
-            
+
             hand[i] = used
-        
+
         for n in hand:
             if not n == used:
                 return False
