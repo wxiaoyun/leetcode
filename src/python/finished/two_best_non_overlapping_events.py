11cdc0f8f895b179d6e20e95d7ff54c4638d0644
wxiaoyun <w.xy020203@gmail.com>
2025-12-23 08:32:31 +0800

    revisit

--- src/python/finished/two_best_non_overlapping_events.py
+++ src/python/finished/two_best_non_overlapping_events.py
@@ -3,40 +3,44 @@ from typing import List
 
 # https://leetcode.com/problems/two-best-non-overlapping-events/
 
+
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        events.sort() # sort by start time in ascending order
-        v_best = 0
-        pqe = [] # min heap by end time: Pq<Tuple<end, value>>
-        best = -1
+        events = [(s, e, v) for s, e, v in events]
+        events.sort()
 
+        best = 0
+        best_left = 0
+        pq = []
         for s, e, v in events:
-          # retrieve all events whose ending time is before the start of the current event
-          while pqe and pqe[0][0] < s:
-            _, v_other = heapq.heappop(pqe)
-            # update best value so far
-            v_best = max(v_best, v_other)
-          
-          best = max(best, v + v_best)
+            while pq and pq[0][0] < s:
+                _, prev_v = heapq.heappop(pq)
+                best_left = max(best_left, prev_v)
 
-          # push the current event to the endtime min heap
-          heapq.heappush(pqe, (e, v))
+            best = max(best, v, best_left + v)
+            heapq.heappush(pq, (e, v))
 
         return best
 
-    # def maxTwoEvents(self, events: List[List[int]]) -> int:
-    #     pq = [(-v, s, e) for (s, e, v) in events]
-    #     heapq.heapify(pq)
 
-    #     best = -1
-    #     for a_start, a_end, a_value in events:
-    #       best = max(best, a_value)
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        events.sort()  # sort by start time in ascending order
+        v_best = 0
+        pqe = []  # min heap by end time: Pq<Tuple<end, value>>
+        best = -1
 
-    #       pqq = copy.deepcopy(pq)
-    #       while pqq:
-    #         v, s, e = heapq.heappop(pqq)
-    #         if s > a_end or e < a_start:
-    #           # print(a_start, a_end, s, e)
-    #           best = max(best, a_value - v)
-    #           break
-    #     return best
+        for s, e, v in events:
+            # retrieve all events whose ending time is before the start of the current event
+            while pqe and pqe[0][0] < s:
+                _, v_other = heapq.heappop(pqe)
+                # update best value so far
+                v_best = max(v_best, v_other)
+
+            best = max(best, v + v_best)
+
+            # push the current event to the endtime min heap
+            heapq.heappush(pqe, (e, v))
+
+        return best
+
