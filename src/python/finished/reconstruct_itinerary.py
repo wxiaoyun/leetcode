c1a22b46b0a955753b586280a47bbfc71cddd1b1
wxiaoyun <w.xy020203@gmail.com>
2025-12-30 11:55:08 +0800

    revisit

--- src/python/finished/reconstruct_itinerary.py
+++ src/python/finished/reconstruct_itinerary.py
@@ -22,3 +22,26 @@ class Solution:
         compute("JFK", out)
         out.reverse()
         return out
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets = sorted(tickets)
+        adj_list = defaultdict(list)
+        for fr, to in tickets:
+            adj_list[fr].append(to)
+        use = defaultdict(int)
+
+        def dfs(node: str, builder: List[str]) -> List[str]:
+            for i, nb in enumerate(adj_list[node]):
+                if i < use[node]:
+                    continue
+                use[node] += 1
+                dfs(nb, builder)
+
+            builder.append(node)
+            return builder
+
+        res = dfs("JFK", [])
+        res.reverse()
+        return res
