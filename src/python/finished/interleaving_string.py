247d3541b48608d1216d76d89d01095e503c8c87
wxiaoyun <w.xy020203@gmail.com>
2025-12-30 11:28:54 +0800

    revisit

--- src/python/finished/interleaving_string.py
+++ src/python/finished/interleaving_string.py
@@ -9,6 +9,31 @@ class Solution:
         if n + m != len(s3):
             return False
 
+        # dp[i][j] = possible to form s3[:i+j] from s1[:i] and s2[:j]
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+
+        # dp[i][j] iff
+        #    dp[i - 1][j] == True and s1[i - 1] == s3[i + j - 1]
+        # or dp[i][j - 1] == True and s2[j - 1] == s3[i + j - 1]
+
+        for i in range(n + 1):
+            for j in range(m + 1):
+                if i - 1 >= 0:
+                    dp[i][j] = dp[i][j] or (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
+                if j - 1 >= 0:
+                    dp[i][j] = dp[i][j] or (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+        return dp[-1][-1]
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n = len(s1)
+        m = len(s2)
+
+        if n + m != len(s3):
+            return False
+
         # dp[i][j]:
         # - Does s1[..i] and s2[..j] form s3[..i + j]
 
