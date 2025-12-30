59b093dbb74655ddec0dfadefe0a25cefafdf5fc
wxiaoyun <w.xy020203@gmail.com>
2025-12-30 11:59:07 +0800

    revisit

--- src/python/finished/maximum_subarray_sum_with_one_deletion.py
+++ src/python/finished/maximum_subarray_sum_with_one_deletion.py
@@ -1,15 +1,29 @@
 from typing import List
 
-
 # https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
+
 
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
-        res = -float('inf')
+        best = -(1 << 31)
+        one_del = best
+        zero_del = best
+
+        for n in arr:
+            # delete 1 element from the zero_del streak or continue the current streak
+            one_del = max(zero_del, one_del + n)
+            zero_del = max(n, zero_del + n)
+            best = max(best, one_del, zero_del)
+        return best
+
+
+class Solution:
+    def maximumSum(self, arr: List[int]) -> int:
+        res = -float("inf")
         # maximum subarray with no deletion
-        best = -float('inf')
+        best = -float("inf")
         # maximum subarray with 1 deletion
-        best_del = -float('inf')
+        best_del = -float("inf")
         for n in arr:
             # either a continue a streak with 1 deleted element
             # or start a new streak with n deleted
@@ -17,4 +31,3 @@ class Solution:
             best = max(best + n, n)
             res = max(res, best, best_del)
         return res
-
