d36874009342af2d27d779c8387afa214fd42b56
wxiaoyun <w.xy020203@gmail.com>
2026-02-08 08:20:59 +0800

    revisit

--- src/python/finished/balanced_binary_tree.py
+++ src/python/finished/balanced_binary_tree.py
@@ -1,38 +1,7 @@
-# Balanced Binary Tree
-# Easy
-# 9K
-# 504
-# Companies
-# Given a binary tree, determine if it is 
-# height-balanced
-# .
-
- 
-
-# Example 1:
-
-
-# Input: root = [3,9,20,null,null,15,7]
-# Output: true
-# Example 2:
-
-
-# Input: root = [1,2,2,3,3,null,null,4,4]
-# Output: false
-# Example 3:
-
-# Input: root = []
-# Output: true
- 
-
-# Constraints:
-
-# The number of nodes in the tree is in the range [0, 5000].
-# -104 <= Node.val <= 104
+from typing import Optional, Tuple
 
 # https://leetcode.com/problems/balanced-binary-tree/
 
-from typing import Optional
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -40,22 +9,39 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def helper(node: Optional[TreeNode]) -> Tuple[bool, int]:
+            if node is None:
+                return True, 0
+
+            left_balance, left_depth = helper(node.left)
+            right_balance, right_depth = helper(node.right)
+
+            return left_balance and right_balance and abs(
+                left_depth - right_depth
+            ) <= 1, max(left_depth, right_depth) + 1
+
+        return helper(root)[0]
+
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def helper(node):
             if node == None:
                 return 0
-            
+
             left_height = helper(node.left)
             right_height = helper(node.right)
 
             if left_height < 0 or right_height < 0:
                 return -1
-            
+
             if abs(left_height - right_height) > 1:
                 return -1
-            
+
             return max(left_height, right_height) + 1
-        
+
         result = helper(root)
         return False if result < 0 else True
