51c11164acfc390398188022426fa8f2db41149b
wxiaoyun <w.xy020203@gmail.com>
2026-03-08 09:16:55 +0800

    revisit

--- src/python/finished/find_unique_binary_string.py
+++ src/python/finished/find_unique_binary_string.py
@@ -2,6 +2,18 @@ from typing import List
 
 # https://leetcode.com/problems/find-unique-binary-string
 
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
+        ans = ["0"] * n
+
+        for i in range(n):
+            if nums[i][i] == "0":
+                ans[i] = "1"
+        return "".join(ans)
+
+
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         N = len(nums[0])
