32aa5d7ba091cdb6137d88ee008c7c93d5acbb59
wxiaoyun <w.xy020203@gmail.com>
2026-04-01 17:50:20 +0800

    lc

---
+++ src/python/revisit/lexicographically_smallest_generated_string.py
@@ -0,0 +1,40 @@
+class Solution:
+    def generateString(self, str1: str, str2: str) -> str:
+        n, m = len(str1), len(str2)
+
+        WILDCARD = "."
+        ans = [WILDCARD] * (n + m - 1)
+
+        for i in range(n):
+            if str1[i] == "F":
+                continue
+
+            for j in range(m):
+                if ans[i + j] != WILDCARD and ans[i + j] != str2[j]:
+                    return ""
+                ans[i + j] = str2[j]
+
+        for i in range(n):
+            if str1[i] == "T":
+                continue
+
+            if any(
+                (ans[i + j] if ans[i + j] != WILDCARD else "a") != str2[j]
+                for j in range(m)
+            ):
+                continue
+
+            unequal = False
+            for j in reversed(range(m)):
+                if ans[i + j] == WILDCARD:
+                    ans[i + j] = "b"
+                    unequal = True
+                    break
+
+            if not unequal:
+                return ""
+
+        for i in range(len(ans)):
+            if ans[i] == WILDCARD:
+                ans[i] = "a"
+        return "".join(ans)
