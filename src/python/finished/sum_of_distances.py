76c6bbabd59bf08a84ecebe0ee1abb641c0db76d
wxiaoyun <w.xy020203@gmail.com>
2026-04-23 19:02:16 +0800

    lc

---
+++ src/python/finished/sum_of_distances.py
@@ -0,0 +1,35 @@
+from typing import List
+
+# https://leetcode.com/problems/sum-of-distances
+
+
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        def helper(nums: List[int]) -> List[int]:
+            N = len(nums)
+            stacks = {}
+            for i in reversed(range(N)):
+                n = nums[i]
+                stk = stacks.setdefault(n, [])
+                stk.append(i)
+
+            ans = [0] * N
+            prefix_count = {}
+            prefix_offset = {}
+            for i in range(N):
+                n = nums[i]
+
+                cur_sum = prefix_count.setdefault(n, 0) * i - prefix_offset.setdefault(n, 0)
+                ans[i] = cur_sum
+
+                prefix_count[n] += 1
+                prefix_offset[n] += i
+
+            return ans
+        
+        ans = helper(nums)
+        rev_ans = helper(list(reversed(nums)))
+        
+        for i, n in enumerate(rev_ans):
+            ans[len(nums) - 1 - i] += n
+        return ans
