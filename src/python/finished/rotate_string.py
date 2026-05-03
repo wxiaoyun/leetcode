f45fe66dedb66210ccd5bf2af2f6b08242bd1b3a
wxiaoyun <w.xy020203@gmail.com>
2026-05-04 07:58:47 +0800

    revisit

--- src/python/finished/rotate_string.py
+++ src/python/finished/rotate_string.py
@@ -2,24 +2,41 @@ from collections import Counter
 
 # https://leetcode.com/problems/rotate-string/
 
+
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s) != len(goal):
-          return False
-        
+            return False
+        for offset in range(len(s)):
+            ok = True
+            for i in range(len(s)):
+                if s[i] == goal[(i + offset) % len(s)]:
+                    continue
+                else:
+                    ok = False
+                    break
+            if ok:
+                return True
+        return False
+
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+
         if Counter(s) != Counter(goal):
-          return False
+            return False
 
         n = len(goal)
         for i in range(n):
-          found_invalid = False
-          for j in range(n):
-            k = (i + j) % n
-            if goal[j] != s[k]:
-              found_invalid = True
-              break
-          
-          if not found_invalid:
-            return True
+            found_invalid = False
+            for j in range(n):
+                k = (i + j) % n
+                if goal[j] != s[k]:
+                    found_invalid = True
+                    break
+
+            if not found_invalid:
+                return True
         return False
-        
