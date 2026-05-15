59e7a267b1efe0c9c2395f0325fc07d1f9e6ce5b
wxiaoyun <w.xy020203@gmail.com>
2026-05-15 09:03:34 +0800

    revisit

--- src/python/finished/find_minimum_in_rotated_sorted_array.py
+++ src/python/finished/find_minimum_in_rotated_sorted_array.py
@@ -4,6 +4,45 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        # case 1: non-rotated, min is at the beginning
+
+        # case 2: rotated, min is after the beginning
+        # 3, 4, 5, 0, 1, 2
+        # if an array is rotated, then everything before the min, is larger than everything after the min
+
+        is_rotated = nums[0] > nums[-1]
+        if not is_rotated or len(nums) == 1:
+            return nums[0]
+
+        l, r = 0, len(nums)
+        ans = -1
+        while l < r:
+            m = l + (r - l) // 2
+            # print()
+            # print(l, m, r)
+            # print(nums[l], nums[m])
+
+            # invariant: l <= pivot < r
+
+            if r - l <= 2:
+                return min(nums[l:r])
+
+            if nums[l] < nums[m]:
+                # turning point is on our right
+                # m <= pivot
+                l = m
+                # print("R")
+            else:  # nums[l] > nums[m]
+                # l <= pivot <= m
+                ans = m
+                r = m + 1
+                # print("L")
+
+        return ans
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
         l = 0
         r = len(nums) - 1
 
@@ -23,6 +62,3 @@ class Solution:
                 r = mid
 
         return False
-            
-
-        
