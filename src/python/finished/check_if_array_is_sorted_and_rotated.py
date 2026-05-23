9d53076486105c057ef244c61ffde542f151f3f5
wxiaoyun <w.xy020203@gmail.com>
2026-05-23 08:09:06 +0800

    revisit

--- src/python/finished/check_if_array_is_sorted_and_rotated.py
+++ src/python/finished/check_if_array_is_sorted_and_rotated.py
@@ -2,12 +2,26 @@ from typing import List
 
 # https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
 
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        incs = 0
+        prev = nums[-1]
+        for n in nums:
+            if prev > n:
+                incs += 1
+            if incs > 1:
+                return False
+            prev = n
+        return True
+
+
 class Solution:
     def check(self, nums: List[int]) -> bool:
         N = len(nums)
         if N < 2:
             return True
-        
+
         first = nums[0]
         prev = nums[0]
 
@@ -16,13 +30,12 @@ class Solution:
             if n < prev:
                 if not first_dec:
                     return False
-                
+
                 first_dec = False
-            
+
             if not first_dec and n > first:
                 return False
 
             prev = n
-        
-        return True
 
+        return True
