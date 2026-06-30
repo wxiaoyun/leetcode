838ba03a2778b2717735b93edde5825babcb5070
wxiaoyun <w.xy020203@gmail.com>
2026-06-30 10:03:04 +0800

    lc

--- src/python/finished/number_of_substrings_containing_all_three_characters.py
+++ src/python/finished/number_of_substrings_containing_all_three_characters.py
@@ -1,5 +1,22 @@
 # https://leetcode.com/problems/number-of-substrings-containing-all-three-characters
 
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        last_index = {ch: -1 for ch in "abc"}
+
+        ans = 0
+        for i, ch in enumerate(s):
+            last_index[ch] = i
+
+            last_all_ch = min(last_index.values())
+            if last_all_ch < 0:
+                continue
+
+            ans += last_all_ch + 1
+        return ans
+
+
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         cnt = {c: 0 for c in "abc"}
@@ -13,8 +30,8 @@ class Solution:
                 cnt[lch] -= 1
                 l += 1
                 lch = s[l]
-            
+
             if min(cnt.values()) > 0:
                 total += l + 1
-        
+
         return total
